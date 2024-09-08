803b35cd2731c13a40c1807b94d154c5b6bc8ebd
xlalurahmatulikhsan <itsmelexiz88@gmail.com>
2024-09-08 23:47:19 +0800

    feat: access-control

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
+import testRoute from "./routes/test.route.js";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
 
 const PORT = 8080;
 app.listen(PORT, () => {
