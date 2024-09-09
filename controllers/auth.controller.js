19bb9dba37fae98be66d3709103efe17fc9c2e51
xlalurahmatulikhsan <itsmelexiz88@gmail.com>
2024-09-10 01:23:01 +0800

    refactor: error to err

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -16,7 +16,7 @@ export const register = async (req, res) => {
     });
 
     res.status(201).json({ message: "User created successfully" });
-  } catch (error) {
+  } catch (err) {
     res.status(500).json({ message: "Failed to create user!" });
   }
 };
@@ -56,7 +56,7 @@ export const login = async (req, res) => {
       })
       .status(200)
       .json(userInfo);
-  } catch (error) {
+  } catch (err) {
     res.status(500).json({ message: "Failed to Login!" });
   }
 };
