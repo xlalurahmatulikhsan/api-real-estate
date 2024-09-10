66854f4fe48e7f5a31596b0117b0c298a06f7490
xlalurahmatulikhsan <itsmelexiz88@gmail.com>
2024-09-10 12:55:12 +0800

    refactor: project structure

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import authRoute from "./routes/auth.route.js";
-import testRoute from "./routes/test.route.js";
-import userRoute from "./routes/user.route.js";
-import postRoute from "./routes/post.route.js";
-import chatRoute from "./routes/chat.route.js";
-import messageRoute from "./routes/message.route.js";
+import authRoute from "./src/routes/auth.route.js";
+import testRoute from "./src/routes/test.route.js";
+import userRoute from "./src/routes/user.route.js";
+import postRoute from "./src/routes/post.route.js";
+import chatRoute from "./src/routes/chat.route.js";
+import messageRoute from "./src/routes/message.route.js";
 
 const app = express();
 
