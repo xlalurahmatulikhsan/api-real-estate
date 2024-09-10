2149b7650920bc027bcc0a664c9fc6c6e933a872
xlalurahmatulikhsan <itsmelexiz88@gmail.com>
2024-09-10 13:07:45 +0800

    updated

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use("/api/posts", postRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
-const PORT = 8080;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} `);
 });
