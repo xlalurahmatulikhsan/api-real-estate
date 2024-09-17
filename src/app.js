ce704b8b0f37723048ac037dc83c5f413143359c
xlalurahmatulikhsan <itsmelexiz88@gmail.com>
2024-09-17 14:37:16 +0800

    feat: add CORS configuration

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
